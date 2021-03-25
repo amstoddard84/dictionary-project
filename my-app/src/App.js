5cc4e39ec50ab8d2a1ab8839bf83788f655c4d07
High_King_Margo <69987194+highkingmargo@users.noreply.github.com>
2021-03-25 19:44:56 -0400

    initial setup

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,13 @@
-import logo from './logo.png';
-import './App.css';
+import logo from "./logo.png";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-     <header>
-        Dictionary App 
+      <header className="App-header">
+      <img src={logo} className="header-logo img-fluid" alt="logo" />
+<h1><i>Define It!</i> App</h1>
       </header>
     </div>
   );
 }
-
-export default App;

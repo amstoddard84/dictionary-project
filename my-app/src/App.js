ad5af54f0ef1abd63c0a9e715b374d63d8470531
High_King_Margo <69987194+highkingmargo@users.noreply.github.com>
2021-03-25 17:55:38 -0400

    imported react

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <header classname="App header">
+        <img src=[logo]
+        alt="logo" classname="img-fluid" />
+        Dictionary App 
+      </header>
+    </div>
+  );
+}
+
+export default App;

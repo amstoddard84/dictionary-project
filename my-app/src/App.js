5e7c05861e4caa51f91cb915c36fe233e82c9330
High_King_Margo <69987194+highkingmargo@users.noreply.github.com>
2021-03-26 18:56:26 -0400

    dictionary initial setup

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,22 @@
 import logo from "./logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
       <img src={logo} className="header-logo img-fluid" alt="logo" />
 <h1><i>Define It!</i> App</h1>
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="text-center">
+        Hand-coded by Anne Marie Robson
+ </footer>
+      </div>
     </div>
   );
 }
